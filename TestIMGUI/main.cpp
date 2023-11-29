d6602a272c59fb610f4f4967724e2b7c76a5de76
Rain <2215338912@qq.com>
2023-11-29 10:38:57 +0800

    Clear imgui

--- TestIMGUI/main.cpp
+++ TestIMGUI/main.cpp
@@ -16,6 +16,7 @@
 GLFWwindow *window;
 
 int main() {
+#pragma region glfw init
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -24,7 +25,9 @@ int main() {
   window = glfwCreateWindow(800, 600, "TestImGUI", nullptr, nullptr);
   glfwMakeContextCurrent(window);
   glfwSwapInterval(0);
+#pragma endregion
 
+#pragma region imgui init
   IMGUI_CHECKVERSION();
   ImGui::CreateContext(nullptr);
   ImGuiIO &io = ImGui::GetIO();
@@ -33,43 +36,24 @@ int main() {
   ImGui::StyleColorsDark();
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL3_Init("#version 330");
-
-  std::string text = "hello world 123";
-  char text_box[50]{"Text box"};
-
-  ImVec4 color;
-
+#pragma endregion
+  
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
     ImGui::Begin("MyImguiWindow");
 
-    ImGui::Text(text.c_str());
-    if (ImGui::Button("MyButton")) {
-      std::cout << "\n Button Down \n";
-      text = "You click the button";
-    }
-
-    ImGui::InputText("Test Text Box", text_box, IM_ARRAYSIZE(text_box));
-
-    ImGui::BeginListBox("xixi");
-    for (size_t i = 0; i < 100; ++i) { if (ImGui::Selectable(std::to_string(i).c_str(), std::to_string(i) == text)) { text = std::to_string(i); } }
-    ImGui::EndListBox();
-
-    if (ImGui::BeginCombo("aa", text.c_str())) {
-      for (int i = 0; i < 100; ++i) { if (ImGui::Selectable(std::to_string(i).c_str())) { text = std::to_string(i); } }
-      ImGui::EndCombo();
-    }
-
-    ImGui::ColorEdit4("Text Color", (float *) &color,ImGuiColorEditFlags_::ImGuiColorEditFlags_AlphaBar);
+ 
 
     ImGui::End();
-
     ImGui::ShowDemoWindow();
+
+    
 
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
