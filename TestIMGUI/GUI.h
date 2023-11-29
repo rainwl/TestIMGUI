663f4aa2ed3b987f6f8adee02133a4290b9630b4
Rain <2215338912@qq.com>
2023-11-29 12:02:59 +0800

    Add imgui getkeydown

--- TestIMGUI/GUI.h
+++ TestIMGUI/GUI.h
@@ -1,13 +1,20 @@
 ﻿#pragma once
 #include "imgui.h"
+
+#include <iostream>
 void draw_gui();
 inline void draw_gui() {
   ImGui::Begin(u8"Debug Window");// u8 for chinese,but do not make sense
   ImGui::Text("hu hu");
 
-  ImDrawList *list = ImGui::GetForegroundDrawList();
-
-  list->AddRectFilled(ImVec2(0, 0), ImGui::GetMousePos(), ImColor(60,255,51));
+  //ImDrawList *list = ImGui::GetForegroundDrawList();
+  //list->AddRectFilled(ImVec2(0, 0), ImGui::GetMousePos(), ImColor(60,255,51));
+  if (ImGui::IsKeyPressed(ImGuiKey_Q)) {
+    std::cout << "xixi";
+  }
+  if (ImGui::IsKeyPressed(static_cast<ImGuiKey>(562))) {
+    std::cout << "hhh";
+  }
 
   ImGui::End();
 }
