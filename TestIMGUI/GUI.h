9cd453f22fde5f94082087053a509c7277adfab4
Rain <2215338912@qq.com>
2023-11-29 11:22:25 +0800

    Add advanced draw

--- TestIMGUI/GUI.h
+++ TestIMGUI/GUI.h
@@ -5,7 +5,9 @@ inline void draw_gui() {
   ImGui::Begin(u8"Debug Window");// u8 for chinese,but do not make sense
   ImGui::Text("hu hu");
 
+  ImDrawList *list = ImGui::GetForegroundDrawList();
 
+  list->AddRectFilled(ImVec2(0, 0), ImGui::GetMousePos(), ImColor(60,255,51));
 
   ImGui::End();
 }
