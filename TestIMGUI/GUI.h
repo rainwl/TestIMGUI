e7b48fe9562236bc4b5476bc3f310c577a1ef2f6
Rain <2215338912@qq.com>
2023-11-29 14:08:06 +0800

    Add Drag

--- TestIMGUI/GUI.h
+++ TestIMGUI/GUI.h
@@ -2,18 +2,51 @@
 #include "imgui.h"
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> drag_list;
+
 void draw_gui();
 inline void draw_gui() {
   ImGui::Begin(u8"Debug Window");// u8 for chinese,but do not make sense
-  ImGui::Text("hu hu");
 
-  //ImDrawList *list = ImGui::GetForegroundDrawList();
-  //list->AddRectFilled(ImVec2(0, 0), ImGui::GetMousePos(), ImColor(60,255,51));
-  if (ImGui::IsKeyPressed(ImGuiKey_Q)) {
-    std::cout << "xixi";
+  for (size_t i = 0; i < 5; ++i) {
+    ImGui::Button(std::to_string(i).c_str());
+    if (i < 5) {
+      ImGui::SameLine();
+    }
+    if (ImGui::BeginDragDropSource()) {
+      ImGui::Text(std::string("Darg: ").append(std::to_string(i).c_str()).c_str());
+      ImGui::SetDragDropPayload("DragIndexButton", &i, sizeof(int));
+      ImGui::EndDragDropSource();
+    }
   }
-  if (ImGui::IsKeyPressed(static_cast<ImGuiKey>(562))) {
-    std::cout << "hhh";
+
+  ImGui::End();
+
+  ImGui::Begin("Drag Window");
+
+  ImGui::Text("Drag Target");
+
+  if (ImGui::BeginDragDropTarget()) {
+    if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("DragIndexButton")) {
+      drag_list.push_back(*(const int *) payload->Data);
+    }
+
+    ImGui::EndDragDropTarget();
+  }
+
+  for (size_t i = 0; i < drag_list.size(); ++i) {
+    //ImGui::Button(std::to_string(drag_list.at(i)).c_str());
+
+    if (ImGui::Button(std::to_string(drag_list.at(i)).c_str())) {
+      drag_list.erase(drag_list.begin() + i);
+    }
+
+    if (i + 1 < drag_list.size()) {
+      ImGui::SameLine();
+    }
   }
 
   ImGui::End();
