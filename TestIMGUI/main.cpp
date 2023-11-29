cd2c762ffa16ad9ab39299268ad4eae5bd84b88a
Rain <2215338912@qq.com>
2023-11-29 08:58:52 +0800

    Update project

---
+++ TestIMGUI/main.cpp
@@ -0,0 +1,30 @@
+﻿#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#pragma comment(lib,"../API/GLFW/glfw3.lib")
+#pragma comment(lib,"../API/GLEW/glew32s.lib")
+#pragma comment(lib,"Opengl32.lib")
+
+
+GLFWwindow *window;
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(800, 600, "TestImGUI", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(0);
+
+
+
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+}
